b7e73bd9da8ed4034417429363063282c3061308
Chapunk0620 <1jhonchristiansolano1@gmail.com>
2024-07-01 21:09:47 +0800

    'asdw'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,9 @@
     <x-slot:heading1>
         Home Page
     </x-slot:heading1>
-    <h1>Welcome to Homepage</h1>
+
+    @foreach ($jobs as $job)
+        <li><strong>{{$job['title']}}:</strong> pays {{$job['salary'] }} per year.</li>
+    @endforeach
+
 </x-layout>
