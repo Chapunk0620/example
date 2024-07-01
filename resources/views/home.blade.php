f4d44af10e7b0c91a30ff1ba8578556eee62dc5a
Chapunk0620 <1jhonchristiansolano1@gmail.com>
2024-07-01 21:16:41 +0800

    'asdwa'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,4 @@
     <x-slot:heading1>
         Home Page
     </x-slot:heading1>
-
-    @foreach ($jobs as $job)
-        <li><strong>{{$job['title']}}:</strong> pays {{$job['salary'] }} per year.</li>
-    @endforeach
-
 </x-layout>
---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot:heading1>
+        Job Listing
+    </x-slot:heading1>
+    @foreach ($jobs as $job)
+        <li><strong>{{$job['title']}}:</strong> pays {{$job['salary'] }} per year.</li>
+    @endforeach
+
+</x-layout>
