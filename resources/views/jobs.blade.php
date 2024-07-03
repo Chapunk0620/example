9efef8ffd4d737a5912657613b8832d6d6b441e1
Chapunk0620 <1jhonchristiansolano1@gmail.com>
2024-07-03 11:10:11 +0800

    Zdas

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,7 +3,9 @@
         Job Listing
     </x-slot:heading1>
     @foreach ($jobs as $job)
-        <li><strong>{{$job['title']}}:</strong> pays {{$job['salary'] }} per year.</li>
+        <li>
+            <strong>{{$job['title']}}:</strong> pays {{$job['salary'] }} per year.
+        </li>
     @endforeach
 
 </x-layout>
