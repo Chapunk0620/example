0237aa5950d9960e202aedd90ca276572cf3fd88
Chapunk0620 <1jhonchristiansolano1@gmail.com>
2024-07-03 13:01:00 +0800

    asd

--- routes/web.php
+++ routes/web.php
@@ -30,26 +30,26 @@ Route::get('/jobs', function () {
 });
 Route::get('/jobs/{id}', function ($id) {
         $jobs = [ 
-                [
-                    'id' => 1,
-                    'title' => 'Director',
-                    'salary' => '$50.000'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Programmer',
-                    'salary' => '$10.000'
-                ],
-                [
-                    'id' => 3,
-                    'title' => 'Teacher',
-                    'salary' => '$40.000'
-                ]
+                    [
+                        'id' => 1,
+                        'title' => 'Director',
+                        'salary' => '$50.000'
+                    ],
+                    [
+                        'id' => 2,
+                        'title' => 'Programmer',
+                        'salary' => '$10.000'
+                    ],
+                    [
+                        'id' => 3,
+                        'title' => 'Teacher',
+                        'salary' => '$40.000'
+                    ]
             ];
 
             $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
 
-            dd($job);
+            
     return view('job', ['job' => $job]);
 });
 Route::get('/contact', function () {
